3da4d7f5b40479074413c00d7feeb0a6fbb29e31
jonathanch96 <jonathanchang96@gmail.com>
2025-03-16 20:26:48 +0700

    course 24 udemy

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -12,8 +12,4 @@ contract Inbox {
     function setMessage(string memory newMessage) public {
         message = newMessage;
     }
-
-    function getMessage() public view returns (string memory){
-        return message;
-    }
 }
